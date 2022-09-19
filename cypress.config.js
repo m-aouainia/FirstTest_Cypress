c9d13740c5abc6417b915194046c6d52fa06474a
m-aouainia <m.aouainia@gmx.de>
2022-09-19 11:21:32 +0200

    New Jenkinsfile

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 's8rzzo',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
